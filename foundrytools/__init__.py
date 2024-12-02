bc5f4dc69424c4a96acf9654c684d651af2e8d75
ftCLI <ftCLI@proton.me>
2024-12-02 14:53:46 +0100

    Refactor imports to use relative paths.

--- foundrytools/__init__.py
+++ foundrytools/__init__.py
@@ -1,6 +1,6 @@
-from foundrytools.font import Font
-from foundrytools.font_finder import FontFinder
-from foundrytools import app, tables
+from .font import Font
+from .font_finder import FontFinder
+from . import app, tables
 
 VERSION = __version__ = "0.1.0"
 __all__ = ["VERSION", "Font", "FontFinder", "tables", "app"]
